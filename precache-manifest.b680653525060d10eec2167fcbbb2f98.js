0275bbf4b191a995c763c5ed1d33f6ebad89701e
Bryan Cage <bacagedeveloper@gmail.com>
2020-04-18 13:59:58 -0400

    Updates

---
+++ precache-manifest.b680653525060d10eec2167fcbbb2f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071f749bea36b3aa9ad5ae742f2b8393",
+    "url": "/urec_app/index.html"
+  },
+  {
+    "revision": "59120673cd809f02d1d3",
+    "url": "/urec_app/static/css/main.35c7b846.chunk.css"
+  },
+  {
+    "revision": "507b3784655cddfcde94",
+    "url": "/urec_app/static/js/2.997129aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/urec_app/static/js/2.997129aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59120673cd809f02d1d3",
+    "url": "/urec_app/static/js/main.84e3a0e9.chunk.js"
+  },
+  {
+    "revision": "b8d00e4a2354f8bad511",
+    "url": "/urec_app/static/js/runtime-main.df35cc92.js"
+  }
+]);
